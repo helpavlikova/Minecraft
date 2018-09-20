cdb1441e7813fb4b8bdb63480be142000defe9d0
Helena <helpavlikova@gmail.com>
2018-09-20 12:52:33 +0200

    dragging works from off side with no spinning

--- Assets/Scripts/OnDrag.cs
+++ Assets/Scripts/OnDrag.cs
@@ -6,17 +6,31 @@ public class OnDrag : MonoBehaviour {
 
     private Vector3 screenPoint;
     private Vector3 offset;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void OnMouseDown()
     {
         screenPoint = Camera.main.WorldToScreenPoint(transform.position);
         offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        
     }
 
     void OnMouseDrag()
     {
         Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
+        //transform.position = curPosition; classical movement
+        transform.position = new Vector3(Mathf.Round(curPosition.x), Mathf.Round(curPosition.y), Mathf.Round(curPosition.z));
+        Debug.Log(transform.position);
+    }
+
+    void Update()
+    {
+        rb.freezeRotation = true; //so that our cube would not rotate all the time
     }
 }
