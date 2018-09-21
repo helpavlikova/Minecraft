1b3730c31b07b79ed3ba03a99cb63aa33381c85f
Helena <helpavlikova@gmail.com>
2018-09-21 20:48:18 +0200

    green cubes can be destroyed by holding down mouse button and standing at the correct angle - need to fix that angle

---
+++ Assets/Scripts/boxScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxScript : MonoBehaviour {
+
+    public int hardness;
+    private bool playerIsClose = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerIsClose && Input.GetMouseButton(0))
+        {
+            hardness--;
+            Debug.Log(hardness);
+        }
+
+        if (hardness <= 0) {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        playerIsClose = true;
+        Debug.Log("Close to box");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        playerIsClose = false;
+        Debug.Log("No longer in close to box");
+    }
+}
